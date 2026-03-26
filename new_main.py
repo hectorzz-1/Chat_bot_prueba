ea7f1cee98c5c19914ac55e33ea77ce5d1c3f568
hectorzz-1 <hector.jlm1006@gmail.com>
2026-03-26 16:44:40 -0600

    Actualizamos lo que pasa si el usuario si tiene agentes disponibles

--- new_main.py
+++ new_main.py
@@ -27,12 +27,12 @@ if __name__ == "__main__":
 
     # Si el usuario no tiene nigún agente disponible
     if not chat_config: 
-        print("No tiene ningún agente creado. Creemos uno")
+        print("you doesn't have any agents created. Let's create one")
         # Inicializamos un nuevo agente
         agent_using = JsonAddConfig(file=JSON_CONFIG).Add_config()
 
         # Le damos un nombre
-        name_agent = input("Nombre: ")
+        name_agent = input("Give him a name: ")
         # Lo guardamos en un diccionario
         name_agent = NameSetter().set(data=name_agent)
         # Lo actualizamos en el diccionario
@@ -41,15 +41,69 @@ if __name__ == "__main__":
         agent_using = UpDateSettingDict(config_agent=agent_using[0], attribute=name_agent).update()
 
         # Le damos un comportamiento
-        behavior_agent = input("Dale un comportamiento: ")
+        behavior_agent = input("Give him a behavior: ")
         # Lo guardamos en un diccionario
         behavior_agent = BehaviorSetter().set(data=behavior_agent)
         # Lo actualizamos en el diccionario
         agent_using = UpDateSettingDict(config_agent=agent_using, attribute=behavior_agent).update()
 
         # Ahora volvemos a cargar el json
-        agents_container = LoadSettingsAgent(file=JSON_CONFIG).load()
+        chat_config = LoadSettingsAgent(file=JSON_CONFIG).load()
         # Actualizamos en el json
-        SaveSettingsAgent(file=JSON_CONFIG, config_list=agents_container, agent_update= agent_using).save()
+        SaveSettingsAgent(file=JSON_CONFIG, config_list=chat_config, agent_update= agent_using).save()
+
+    # Si el usuario tiene agentes para usar
+    if chat_config:
+        # Lista donde estaran los agente que se pueden usar
+        AGENTS_USED = []
+        # Mostrar los diferentes agentes disponibles en pantalla
+        for agent in chat_config:
+            print(agent["name"])
+            # Guardamos los nombres e ids en una lista
+            AGENTS_USED.append({"name":agent["name"], "id" : agent["id_chat"]})
+        # Damos la opción de crear uno nuevo
+        print("create")
+
+        # Seleccionar un agente
+        agent_name = input("Choose the one you want to use: ") 
+
+        # Si el usuario quiere crear un nuevo chat bot
+        if agent_name == "create":
+            # creamos un nuevo agente
+            agent_using = JsonAddConfig(file=JSON_CONFIG).Add_config()
+
+            # Le damos un nombre
+            name_agent = input("Give him a name: ")
+            # Lo guardamos en un diccionario
+            name_agent = NameSetter().set(data=name_agent)
+            # Lo actualizamos en el diccionario
+            agent_using = UpDateSettingDict(config_agent=agent_using, attribute=name_agent).update()
+
+            # Le damos un comportamiento
+            behavior_agent = input("Give him a behavior: ")
+            # Lo guardamos en un diccionario
+            behavior_agent = BehaviorSetter().set(data=behavior_agent)
+            # Lo actualizamos en el diccionario
+            agent_using = UpDateSettingDict(config_agent=agent_using, attribute=behavior_agent).update()
+
+            # Ahora volvemos a cargar el json
+            agents_container = LoadSettingsAgent(file=JSON_CONFIG).load()
+            # Actualizamos en el json
+            SaveSettingsAgent(file=JSON_CONFIG, config_list=agents_container, agent_update= agent_using).save()
+        
+        # Informar si puso un agente inexistente
+        elif agent_name not in [agent["name"] for agent in AGENTS_USED]:
+            raise ValueError(f"Error: The {agent_name} agent does not exist. Please choose a valid one")
+
+        # Si el usuario puso un agente correcto
+        elif agent_name in [agent["name"] for agent in AGENTS_USED]:
+            # Obtener el id
+            agent_id = next(
+                (agent["id"] for agent in AGENTS_USED if agent["name"] == agent_name)
+            )
+            # Definir el agente que usará el usuario
+            agent_using = next(
+                (agent for agent in chat_config if agent["id_chat"] == agent_id)
+            )
 
     
